99860aff22eaf80da70371217c11dff4f5635777
rchdlps <rchdlps@gmail.com>
2018-08-06 19:59:37 -0300

    change ignore's

--- microblog.py
+++ microblog.py
@@ -8,3 +8,7 @@ cli.register(app)
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 'Post': Post}
+
+
+if __name__ == '__main__':
+    app.run(debug=True, use_debugger=False, use_reloader=False, passthrough_errors=True)
